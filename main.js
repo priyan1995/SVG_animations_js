09291b581fbc302edcaaaf943dcc6b6a742a5b34
priyan1995 <priyan.abewickrama@ogilvy.com>
2022-02-10 14:40:49 +0530

    animate on click heart

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var paths = document.querySelectorAll("svg path");
+var nextTxt = document.querySelector(".next-text path");
+var heartIcon = document.getElementById("heart");
+const pathHeart = document.querySelector("#heart path");
 
 i = 0;
 
@@ -14,6 +17,37 @@ paths.forEach(function(item, index) {
 })
 
 window.addEventListener('DOMContentLoaded', () => {
-    const pathHeart = document.querySelector("#heart path");
-    console.log(pathHeart.getTotalLength());
+
+    const likeUnlikePost = () => {
+        if (pathHeart.classList.contains("liked")) {
+            pathHeart.classList.remove("liked");
+            pathHeart.classList.add("unliked");
+        } else {
+            pathHeart.classList.remove("unliked");
+            pathHeart.classList.add("liked");
+        }
+    }
+
+    pathHeart.addEventListener('click', likeUnlikePost);
 })
+
+console.log(nextTxt.getTotalLength())
+
+
+heartIcon.addEventListener('click', addAnimationNext);
+
+function addAnimationNext() {
+
+    if (pathHeart.classList.contains("liked")) {
+        setInterval(() => {
+            nextTxt.classList.add('next-txt-animate');
+            nextTxt.classList.re4move('next-text-animation-reverse');
+        }, 2000)
+    } else if (pathHeart.classList.contains("unliked")) {
+        nextTxt.classList.remove('next-txt-animate');
+        nextTxt.classList.add('next-text-animation-reverse');
+    }
+
+
+
+}
